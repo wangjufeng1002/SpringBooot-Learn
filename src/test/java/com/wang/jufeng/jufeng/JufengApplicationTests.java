10564bbe5ff727f9c7b4f738e29193c68cc7cb7f
jufeng.wang <1258454532@qq.com>
2021-01-11 10:52:59 +0800

    Initial commit

---
+++ src/test/java/com/wang/jufeng/jufeng/JufengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.jufeng.jufeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JufengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
